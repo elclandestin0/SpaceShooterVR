47ed0a4a3d983901478474a82db2906778a270a9
Ihcenech <ihcene.ch@gmail.com>
2016-04-30 17:46:46 -0400

    Adding the Asteroids and shooting

---
+++ CardboardTest2/Assets/Cardboard/Scripts/EnemySpawn.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawn : MonoBehaviour {
+
+	public GameObject EnemyBarrel; 
+
+	public GameObject Enemy; 
+
+	public float countdown = 150.0f;
+    private System.Random ran;
+
+	//public GameObject temporaryBulletHandler;
+
+
+	public float forwardForce;
+
+	// Use this for initialization
+	void Start () {
+        ran = new System.Random();
+        
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//float time = 0.5f; 
+		countdown -= Time.deltaTime; 
+		if (countdown <= 0.0f) 
+		{
+			GameObject temporaryEnemyHandler;
+
+			temporaryEnemyHandler = Instantiate (Enemy, EnemyBarrel.transform.position, EnemyBarrel.transform.rotation) as GameObject;
+
+			temporaryEnemyHandler.transform.Rotate (Vector3.left * 90);
+
+            temporaryEnemyHandler.transform.position = new Vector3(ran.Next(20), ran.Next(20), 40);
+
+			Rigidbody temporaryRigidBody;
+			temporaryRigidBody = temporaryEnemyHandler.GetComponent<Rigidbody> ();
+
+			temporaryRigidBody.AddForce (transform.forward * forwardForce);
+
+			Destroy (temporaryEnemyHandler, 5.0f);
+
+            countdown = 0.05f;
+
+		}
+	}
+
+
+}
