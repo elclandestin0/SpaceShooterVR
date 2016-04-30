134a14f9c17452a2de30ab6353a4871e67bfaa9a
Roberto Ruffolo <robertoruffolo@hotmail.com>
2016-04-30 11:55:21 -0400

    First Commit

---
+++ CardboardTest2/Assets/Cardboard/Scripts/BulletCollision.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletCollision : MonoBehaviour 
+{
+	public GameObject Cube; 
+	public GameObject Bullet; 
+	
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.name == "Cube") 
+		{
+			GetComponent<Renderer> ().material.color = Color.blue;
+		}
+
+	}
+}
---
+++ CardboardTest2/Assets/Cardboard/Scripts/Fire.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fire : MonoBehaviour {
+
+	public GameObject BulletBarrel; 
+
+	public GameObject Bullet; 
+
+	//public GameObject temporaryBulletHandler;
+
+
+	public float forwardForce;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+
+			GameObject temporaryBulletHandler;
+
+			temporaryBulletHandler = Instantiate (Bullet, BulletBarrel.transform.position, BulletBarrel.transform.rotation) as GameObject;
+
+			temporaryBulletHandler.transform.Rotate (Vector3.left * 90); 
+
+			Rigidbody temporaryRigidBody;
+			temporaryRigidBody = temporaryBulletHandler.GetComponent<Rigidbody> ();
+
+			temporaryRigidBody.AddForce (transform.forward * forwardForce);
+
+			Destroy (temporaryBulletHandler, 3.0f);
+		}
+}
+
+
+}
